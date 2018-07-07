aaa96c085dec4a3ee97c1a7dc0c81a2ad0a0365c
mekilis <st.nonso@gmail.com>
2018-07-07 17:07:00 +0100

    clean function added

--- cmd/purify/main.go
+++ cmd/purify/main.go
@@ -1,4 +1,27 @@
 package main
 
+import "github.com/mekilis/purify/pkg/chatterbox"
+
 func main() {
+}
+
+// Clean does the actual job of filtering a given string of possible profane
+// words
+func Clean(chatterBox *chatterbox.ChatterBox, wordsSlice []string) (int, int) {
+	var goodWords, badWords int
+	for i, word := range wordsSlice {
+		if chatterBox.BadWords.FindWord(word) {
+			w := string(word[0])
+			k := len(word)
+			for j := 2; j < k; j++ {
+				w += "*"
+			}
+			w += string(word[k-1])
+			wordsSlice[i] = w
+			badWords++
+		} else {
+			goodWords++
+		}
+	}
+	return goodWords, badWords
 }
